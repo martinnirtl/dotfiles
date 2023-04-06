0f1a7774d0466665241da16a1496592fd821a198
Martin Nirtl <martin@nirtl.rocks>
2023-04-06 16:36:02 +0200

    rm emojis and update scripts

---
+++ run_once_after_iterm-info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo
+echo "░▀█▀░▀█▀░█▀▀░█▀▄░█▄█░▀▀▄"
+echo "░░█░░░█░░█▀▀░█▀▄░█░█░▄▀░"
+echo "░▀▀▀░░▀░░▀▀▀░▀░▀░▀░▀░▀▀▀"
+echo
+
+echo "After iTerm2 installation:"
+echo "> Configure dynamic profiles folder to be loaded from $HOME/.config/iterm2/DynamicProfiles (see Settings > Advanced > Section General > Path to folder with dynamic profiles)"
+echo "> Set default profile in iterm2 (see Settings > Profiles)"
