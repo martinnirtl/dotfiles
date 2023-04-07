f1445e0fe3612dd747fd7a96bddc170a3cffaeeb
Martin Nirtl <martin@nirtl.rocks>
2023-04-07 15:18:08 +0200

    rename scripts

---
+++ run_once_after_init-info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF
+    ____                 __   ________    _      __
+   / __ \___  ____ _____/ /  /_  __/ /_  (_)____/ /
+  / /_/ / _ \/ __ '/ __  /    / / / __ \/ / ___/ / 
+ / _, _/  __/ /_/ / /_/ /    / / / / / / (__  )_/  
+/_/ |_|\___/\__,_/\__,_/    /_/ /_/ /_/_/____(_)   
+
+EOF                                                   
+
+echo "After iTerm2 installation:"
+echo "> Configure dynamic profiles folder to be loaded from $HOME/.config/iterm2/DynamicProfiles (see Settings > Advanced > Section General > Path to folder with dynamic profiles)"
+echo "> Set default profile in iterm2 (see Settings > Profiles)"
