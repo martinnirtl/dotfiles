cfb1865cf4318b889864a37b3e85b27705a3d40f
Martin Nirtl <martin@nirtl.rocks>
2023-08-21 21:25:06 +0200

    fix promt func

--- updaters/helpers/utils.sh
+++ updaters/helpers/utils.sh
@@ -7,15 +7,22 @@ require_sudo() {
     fi
 }
 
-prompt_to_continue_options() {
+prompt_to_continue_yn() {
     local question="$1"
     local default_answer="$2"
-    local exit_code="$3"
-    local exit_message="$4"
+    local options="y/N"
+
+    if [[ -z "$default_answer" ]]; then
+        answer="N"
+    fi
+
+    if [[ "$default_answer" == "y" || "$default_answer" == "Y" ]]; then
+        options="Y/n"
+    fi
 
     # Prompt user with a timeout and wait for a single character input or Enter key
     read -t 10 -n 1 -p "$question (${default_answer^^}/n) " answer
-    echo ""  # Move to a new line after the input
+    # echo ""  # Move to a new line after the input
 
     # If answer is empty (due to timeout or Enter key), set it to the default answer
     if [[ -z "$answer" ]]; then
@@ -24,8 +31,8 @@ prompt_to_continue_options() {
 
     # Check the answer and act accordingly
     if [[ $answer == "n" || $answer == "N" ]]; then
-        echo "$exit_message"
-        exit "$exit_code"
+        echo " > cancelled"
+        exit 0
     fi
 }
 
