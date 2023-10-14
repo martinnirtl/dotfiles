b6f05b4c7606fe008b35734305536cee69f84c32
Martin Nirtl <martin@nirtl.rocks>
2023-10-14 21:58:21 +0200

    merge scripts

--- run_once_before_init.sh
+++ run_once_before_init.sh
@@ -74,3 +74,22 @@ if ! [ -d "$ZSH_PLUGIN" ]; then
 else
   echo "Skipping zsh-syntax-highlighting. Already installed..."
 fi
+
+# TODO add to ~/.ssh/config
+# Host *
+# 	IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+# TODO change remote url of chezmoi git to be ssh based
+# git remote set-url origin git@github.com/martinnirtl/dotfiles.git
+
+cat << EOF
+    ____                 __   ________    _      __
+   / __ \___  ____ _____/ /  /_  __/ /_  (_)____/ /
+  / /_/ / _ \/ __ '/ __  /    / / / __ \/ / ___/ / 
+ / _, _/  __/ /_/ / /_/ /    / / / / / / (__  )_/  
+/_/ |_|\___/\__,_/\__,_/    /_/ /_/ /_/_/____(_)   
+
+EOF                                                   
+
+echo "After iTerm2 installation:"
+echo "> Configure dynamic profiles folder to be loaded from $HOME/.config/iterm2/DynamicProfiles (see Settings > Advanced > Section General > Path to folder with dynamic profiles)"
+echo "> Set default profile in iterm2 (see Settings > Profiles)"
