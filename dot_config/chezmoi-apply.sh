553f72cd471b7e9e6e03f1ea9fe02b693902d1fa
Martin Nirtl <martin.nirtl@gmail.com>
2025-08-23 08:23:19 +0200

    add color

--- dot_config/chezmoi-apply.sh
+++ dot_config/chezmoi-apply.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+CYAN='\033[38;5;51m'    # Bright cyan (xterm 51)
+NC='\033[0m'            # Reset to default color
+
 # CHEZMOI APPLY CHECKER
 LAST_APPLY=$(date -j -r $HOME/.config/chezmoi_last-apply.txt +"%s")
 LAST_APPLY_READABLE=$(date -j -f "%s" $LAST_APPLY)
 if [ $(date -v -7d +"%s") -ge $LAST_APPLY ]; then
-  echo "Run 'chezmoi apply' (last apply: $LAST_APPLY_READABLE)? Y/n"
+  echo -e "${CYAN}Run 'chezmoi apply' (last apply: $LAST_APPLY_READABLE)?${NC} Y/n"
   while true; do
     read -sn 1 yn
     case $yn in
